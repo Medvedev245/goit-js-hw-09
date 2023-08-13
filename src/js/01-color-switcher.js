99e4ed0b75bcdb705cba42767637ac4ce96a0a49
Medvedev245 <56516959+Medvedev245@users.noreply.github.com>
2023-08-13 18:08:19 +0200

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  bodyColor: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+// ===========================================
+let timerId = null;
 
+refs.btnStart.addEventListener('click', () => {
+  refs.btnStart.disabled = true;
+  timerId = setInterval(() => {
+    refs.bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+refs.btnStop.addEventListener('click', () => {
+  refs.btnStart.disabled = false;
+  clearInterval(timerId);
+});
+// ===========================================
+// console.dir(refs.btnStart);
+// console.log(refs.btnStop);
+// console.dir(refs.bodyColor.style.backgroundColor);
+// ===========================================
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+console.log(getRandomHexColor());
+
+// ===========================================
+
+// ===========================================
